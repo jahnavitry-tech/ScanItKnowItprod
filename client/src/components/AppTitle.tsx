e786adb8f12b39f61a3f5118abacbb388b06afb1
jahnavitry-tech <jahnavitry@gmail.com>
2025-11-12 22:24:32 -0600

    feat: Add dark mode support for app title with separate PNG images

--- client/src/components/AppTitle.tsx
+++ client/src/components/AppTitle.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 const AppTitle: React.FC = () => {
   return (
-    <img 
-      src="/assets/app-title.png" 
-      alt="App Title" 
-      width="240" 
-      height="42" 
-      className="w-60 h-10"
-    />
+    <>
+      <img 
+        src="/assets/app-title.png" 
+        alt="App Title" 
+        width="240" 
+        height="42" 
+        className="w-60 h-10 dark:hidden"
+      />
+      <img 
+        src="/assets/app-title-dark.png" 
+        alt="App Title" 
+        width="240" 
+        height="42" 
+        className="w-60 h-10 hidden dark:block"
+      />
+    </>
   );
 };
 
